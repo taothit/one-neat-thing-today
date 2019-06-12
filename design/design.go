8b2a49c030ad91dd436ba59b2168ecfd28f64cd4
Aaron Douthit <aaron.douthit@gmail.com>
2019-06-11 20:56:31 -0500

    Converted to goa.design v3 dsl.

--- design/design.go
+++ design/design.go
@@ -7,7 +7,6 @@ import (
 var _ = API("discover", func() {
 	Title("One-new-thing-today")
 	Description("Discover one new thing a day.")
-	Produces("application/json")
 	Server("discoverysvr", func() {
 		Services("neatThing")
 		Host("localhost", func() {
@@ -21,12 +20,51 @@ var _ = Service("neatThing", func() {
 	Description("Allows access to discover neat things.")
 
 	Method("neatThingToday", func() {
-		Result(Int)
-
 		HTTP(func() {
 			GET("/neat/thing/today")
 		})
+
+		Result(NeatThingMedia)
 	})
 
 	Files("/openapi.json", "../../gen/http/openapi.json")
+})
+
+// NeatThingMedia represents a neat thing with its name, description/definition,
+// and a great illustrative link.
+var NeatThingMedia = Type("application/vnd.douthitlab.neatthing", func() {
+	TypeName("NeatThing")
+	Field(1, "name", String, "The neat thing")
+	Field(2, "definition", String, "What the neat thing is")
+	Field(3, "link", String, "Illustrative link for the neat thing", func() {
+		Format(FormatURI)
+	})
+	Field(4, "date", String, "When this was a neat thing", func() {
+		Format(FormatDateTime)
+	})
+	Field(5, "bibliography", ArrayOf(String))
+
+	View("default", func() {
+		Attribute("name")
+		Attribute("definition")
+		Attribute("link")
+	})
+	View("full", func() {
+		Attribute("name")
+		Attribute("definition")
+		Attribute("link")
+		Attribute("date")
+		Attribute("bibliography")
+	})
+	View("name", func() {
+		Attribute("name")
+	})
+	View("name+definition", func() {
+		Attribute("name")
+		Attribute("definition")
+	})
+	View("name+link", func() {
+		Attribute("name")
+		Attribute("link")
+	})
 })
