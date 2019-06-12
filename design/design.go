9075124b3f475b53fbd7a84082457ff60635e9e4
Aaron Douthit <aaron.douthit@gmail.com>
2019-06-11 19:49:50 -0500

    SAVEPOINT

--- design/design.go
+++ design/design.go
@@ -1,68 +1,32 @@
 package design
 
 import (
-	. "github.com/goadesign/goa/design"
-	. "github.com/goadesign/goa/design/apidsl"
+	. "goa.design/goa/v3/dsl"
 )
 
-var _ = API("Discovery", func() {
-	Title("A neat thing each day")
-	Description("Discover something neat each day by being given an item from a curated list.")
-	Host("localhost:8080")
-	Scheme("http")
-})
-
-var _ = Resource("neatThing", func() {
-	Description("Something neat for you")
-	DefaultMedia(NeatThingMedia)
-	BasePath("/neat/thing")
-	Action("today", func() {
-		Routing(GET("today"))
-		Description("GET the neat thing for the day")
-		Response(OK, NeatThingMedia)
+var _ = API("discover", func() {
+	Title("One-new-thing-today")
+	Description("Discover one new thing a day.")
+	Produces("application/json")
+	Server("discoverysvr", func() {
+		Services("neatThing")
+		Host("localhost", func() {
+			URI("http://localhost:8000")
+			URI("grpc://localhost:8080")
+		})
 	})
 })
 
-var _ = Resource("swagger", func() {
-	Origin("*", func() {
-		Methods("GET")
-	})
-	Files("/swagger.json", "swagger/swagger.json")
-})
+var _ = Service("neatThing", func() {
+	Description("Allows access to discover neat things.")
 
-// NeatThingMedia represents a neat thing with its name, description/definition,
-// and a great illustrative link.
-var NeatThingMedia = MediaType("application/vnd.douthitlab.neatthing", func() {
-	TypeName("NeatThing")
-	ContentType("application/json")
-	Attributes(func() {
-		Attribute("name", String, "The neat thing")
-		Attribute("definition", String, "What the neat thing is")
-		Attribute("link", String, "Illustrative link for the neat thing")
-		Attribute("date", DateTime, "When this was a neat thing")
-		Attribute("bibliography", ArrayOf(String))
+	Method("neatThingToday", func() {
+		Result(Int)
+
+		HTTP(func() {
+			GET("/neat/thing/today")
+		})
 	})
-	View("default", func() {
-		Attribute("name")
-		Attribute("definition")
-		Attribute("link")
-	})
-	View("full", func() {
-		Attribute("name")
-		Attribute("definition")
-		Attribute("link")
-		Attribute("date")
-		Attribute("bibliography")
-	})
-	View("name", func() {
-		Attribute("name")
-	})
-	View("name+definition", func() {
-		Attribute("name")
-		Attribute("definition")
-	})
-	View("name+link", func() {
-		Attribute("name")
-		Attribute("link")
-	})
+
+	Files("/openapi.json", "../../gen/http/openapi.json")
 })
--- go.mod
+++ go.mod
@@ -4,10 +4,12 @@ go 1.12
 
 require (
 	github.com/armon/go-metrics v0.0.0-20190430140413-ec5e00d3c878 // indirect
-	github.com/dimfeld/httptreemux v5.0.1+incompatible // indirect
-	github.com/goadesign/goa v1.4.1
+	github.com/goadesign/goa v2.0.0+incompatible
 	github.com/gofrs/uuid v3.2.0+incompatible // indirect
-	github.com/gorilla/websocket v1.4.0 // indirect
-	github.com/spf13/cobra v0.0.5
-	goa.design/goa v2.0.0+incompatible // indirect
+	github.com/golang/protobuf v1.3.1
+	github.com/grpc-ecosystem/go-grpc-middleware v1.0.0
+	github.com/onsi/ginkgo v1.8.0 // indirect
+	github.com/onsi/gomega v1.5.0 // indirect
+	goa.design/goa/v3 v3.0.2
+	google.golang.org/grpc v1.20.1
 )
